a6f103c8ef565209826230cb4fb0dbd39217d3b7
Rajon Dash <cse_1922020022@lus.ac.bd>
2025-03-23 13:41:30 +0600

    frist

--- db.js
+++ db.js
@@ -1,31 +1,18 @@
-// const mysql = require("mysql2");
-
-// const db = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
-
-// db.connect((err) => {
-//   if (err) {
-//     console.error("Database connection failed: ", err);
-//   } else {
-//     console.log("Connected to MySQL database!");
-//   }
-// });
-
-// module.exports = db;
-
 
 const mysql = require("mysql2");
 
+
 // Create a MySQL connection
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  // host: process.env.DB_HOST,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 // Connect to the database
